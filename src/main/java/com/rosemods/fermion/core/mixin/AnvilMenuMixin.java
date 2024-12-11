14e0949dd1de347ef6b4cc5c8a7c2ffd6dad7dde
rosevcook <rose.cook969@gmail.com>
2024-12-11 12:10:37 +0000

    Update AnvilMenuMixin.java

--- src/main/java/com/rosemods/fermion/core/mixin/AnvilMenuMixin.java
+++ src/main/java/com/rosemods/fermion/core/mixin/AnvilMenuMixin.java
@@ -1,16 +1,26 @@
 package com.rosemods.fermion.core.mixin;
 
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AnvilMenu;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AnvilMenu.class)
-public class AnvilMenuMixin {
+public abstract class AnvilMenuMixin {
+    @Shadow
+    public abstract int getCost();
+
     @Inject(method = "getCost", at = @At("HEAD"), cancellable = true)
     private void getCost(CallbackInfoReturnable<Integer> info) {
         info.setReturnValue(0);
+    }
+
+    @Inject(method = "mayPickup", at = @At("HEAD"), cancellable = true)
+    private void mayPickup(Player player, boolean p_39024_, CallbackInfoReturnable<Boolean> info) {
+        info.setReturnValue(player.getAbilities().instabuild || player.experienceLevel >= this.getCost());
     }
 
 }
